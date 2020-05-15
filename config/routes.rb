eec10fcbf79c6dab15c50a6acbfeeefa6ed6637d
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-05-16 01:40:06 +0900

    add profile-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to:"pages#home"
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   # asオプションを使うと、ルーティングに名前をつけれる
   get '/users/:id', to:'users#show', as: 'user'
