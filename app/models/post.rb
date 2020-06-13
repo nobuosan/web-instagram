f2b735ddd10a31cdeb4d067afd676c0cddf5ebe6
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-06-13 12:37:03 +0900

    add likes db

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+
 
   has_many :photos, dependent: :destroy
   # postを通してphotosを保存する
