9e431db6d808ea5cc47c43f4d5b8f76b20d8d150
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-06-14 19:11:45 +0900

    add comment-destroy-custom

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,6 @@ class Post < ApplicationRecord
 
   accepts_nested_attributes_for :photos
 
-  # ==========ここから追加する==========
   def liked_by(current_user)
     # user_idとpost_idが一致するlikeを検索する
     Like.find_by(user_id: current_user.id, post_id: id)
