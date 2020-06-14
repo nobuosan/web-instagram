34b09b90d187aec67e321d71e289ed1d2077e7cb
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-06-14 20:01:43 +0900

    edeit migrate

--- db/migrate/20200515134653_add_name_to_user.rb
+++ db/migrate/20200515134653_add_name_to_user.rb
@@ -2,7 +2,5 @@ class AddNameToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :name, :string,null: false, default: ""
 
-    #validation
-    validates :name, presence: true, length:{ maximum:50 }
   end
 end
